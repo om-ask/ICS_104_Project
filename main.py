29f92d66739123632c04fd934cfd2fb74c10e84a
hashumshum <55711149+hashumshum@users.noreply.github.com>
2024-04-03 03:45:00 +0300

    updateFile و شوية من main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from readFileToDic import read_file_to_dic
 from showData import show_data
 from menu import display_menu, wrap_function
+from updateFile import update_file
 
 STUDENT_FILE_NAME = "students.txt"
 
@@ -14,7 +15,8 @@ def main():
         "Add Record": wrap_function(add_record, student_records),
         "Remove Record": wrap_function(remove_record, student_records),
         "Modify Record": wrap_function(modify_record, student_records),
-        "Sort Records": wrap_function(sort_records, student_records)
+        "Sort Records": wrap_function(sort_records, student_records),
+        "Save And Exit" : wrap_function(updateFile,student_records,STUDENT_FILE_NAME)
     }
 
     display_menu(main_menu_options, "Choose:", "Thank you")
--- updateFile.py
+++ updateFile.py
@@ -1,2 +1,9 @@
-def update_file(student_records: dict, file):
-    pass
+def update_file(student_records: dict, file_name: str):
+    lines = []
+
+    for student_id in student_records:
+        lines.append(f"{student_id},{student_records[student_id]['name']},{student_records[student_id]['gpa']}")
+
+    file = open(file_name,"w")
+    for line in lines:
+        file.write(line + '\n')
